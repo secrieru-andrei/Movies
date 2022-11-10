57a479ea38e94fbf5cd40bcec97bbea80e2056c7
Secrieru Andrei <secrieru.andrei@yahoo.com>
2022-11-10 21:16:36 +0200

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Secrieru Andrei on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
